732f4c424da4e67d3260c5810bdbae9353ccb7a2
Mazlina <118154334+OttoDesu@users.noreply.github.com>
2026-01-07 21:40:13 +0800

    Delete Project_WebDev directory

--- Project_WebDev/assets/app.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const flashes = document.querySelectorAll('.alert');
-  flashes.forEach(f => setTimeout(() => f.classList.add('fade'), 4000));
-});
